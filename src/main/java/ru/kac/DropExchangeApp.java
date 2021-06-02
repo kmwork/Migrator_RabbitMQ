5672a296679a3b4ce285301e5e456af764b217ed
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-02 21:48:28 +0300

    SSL рукопожатие успешно выполнено

--- src/main/java/ru/kac/DropExchangeApp.java
+++ src/main/java/ru/kac/DropExchangeApp.java
@@ -9,8 +9,7 @@ public class DropExchangeApp {
 
     public static void main(String[] argv) throws Exception {
         log.info("[DropExchangeApp] start");
-        try (Connection connection = MqUtils.createMqConnection()) {
-            Channel channel = connection.createChannel();
+        try (Connection connection = MqUtils.createMqConnection(); Channel channel = connection.createChannel()) {
             channel.exchangeDelete(MqUtils.getOutExchange());
         }
         log.info("[DropExchangeApp] finish");
