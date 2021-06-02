6beeba5050fa1cf03d3d2835f739254d7213b325
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-02 13:10:53 +0300

    init for rabbitmq proba

--- src/main/java/ru/kac/SslUtils.java
+++ src/main/java/ru/kac/SslUtils.java
@@ -1,23 +1,41 @@
 package ru.kac;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.KeyStore;
-import java.security.SecureRandom;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.crypto.EncryptedPrivateKeyInfo;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 
+@Slf4j
 public class SslUtils {
 
     public static AppSslContextFactory createSslFactory() throws Exception {
         try {
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-
-            KeyManager[] keyManager = getKeyManager("pkcs12", "src/main/resources/rabbitmq_keys/client_key.p12", "bunnies");
-            TrustManager[] trustManager = getTrustManager("jks", "path/to/CA.truststore", "trust_store_password");
-            sslContext.init(keyManager, trustManager, new SecureRandom());
-
+//            SSLContext sslContext = SSLContext.getInstance("TLS");
+//            System.setProperty("javax.net.ssl.trustStore","rabbitmq_keys/ca_certificate.pem");
+//
+//            KeyManager[] keyManager = getKeyManager("pkcs12", "src/main/resources/rabbitmq_keys/client_key.p12", "bunnies");
+//            TrustManager[] trustManager = getTrustManager("jks", "path/to/CA.truststore", "trust_store_password");
+//            sslContext.init(keyManager, trustManager, new SecureRandom());
+            SSLContext sslContext = createSslContext();
             return new AppSslContextFactory(sslContext);
         } catch (Exception e) {
             throw new RuntimeException("Unable to setup keystore and truststore", e);
@@ -55,57 +73,77 @@ public class SslUtils {
 
         return tmf.getTrustManagers();
     }
-//    private SSLContext createSslContext() throws Exception {
-//
-//        String pem = IOUtils.resourceToString("rabbitmq_keys/client_key.pem", StandardCharsets.UTF_8);
-//        Pattern parse = Pattern.compile("(?m)(?s)^---*BEGIN ([^-]+)---*$([^-]+)^---*END[^-]+-+$");
-//        Matcher m = parse.matcher(pem);
-//        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-//        Base64.Decoder decoder = Base64.getMimeDecoder();
-//        List<Certificate> certList = new ArrayList<>(); // java.security.cert.Certificate
-//
-//        PrivateKey privateKey = null;
-//
-//        int start = 0;
-//        while (m.find(start)) {
-//            String type = m.group(1);
-//            String base64Data = m.group(2);
-//            byte[] data = decoder.decode(base64Data);
-//            start += m.group(0).length();
-//            type = type.toUpperCase();
-//            if (type.contains("CERTIFICATE")) {
-//                Certificate cert = certFactory.generateCertificate(new ByteArrayInputStream(data));
-//                certList.add(cert);
-//            } else if (type.contains("RSA PRIVATE KEY")) {
-//                // TODO: load and parse PKCS1 data structure to get the RSA private key
-//                privateKey = ...
-//            } else {
-//                System.err.println("Unsupported type: " + type);
-//            }
-//
-//        }
-//        if (privateKey == null)
-//            throw new RuntimeException("RSA private key not found in PEM file");
-//
-//        char[] keyStorePassword = new char[0];
-//
-//        KeyStore keyStore = KeyStore.getInstance("JKS");
-//        keyStore.load(null, null);
-//
-//        int count = 0;
-//        for (Certificate cert : certList) {
-//            keyStore.setCertificateEntry("cert" + count, cert);
-//            count++;
-//        }
-//        Certificate[] chain = certList.toArray(new Certificate[certList.size()]);
-//        keyStore.setKeyEntry("key", privateKey, keyStorePassword, chain);
-//
-//        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-//        tmf.init(keyStore);
-//        KeyManagerFactory kmf = KeyManagerFactory.getInstance("RSA");
-//        kmf.init(keyStore, keyStorePassword);
-//        SSLContext sslContext = SSLContext.getInstance("TLS");
-//        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
-//        return sslContext;
-//    }
+
+    private static byte[] readFileFromPemFile(String nameTag, String fileResourceName) throws Exception {
+        String pem = IOUtils.resourceToString(fileResourceName, StandardCharsets.UTF_8);
+        Pattern parse = Pattern.compile("(?m)(?s)^---*BEGIN ([^-]+)---*$([^-]+)^---*END[^-]+-+$");
+        Matcher m = parse.matcher(pem);
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        Base64.Decoder decoder = Base64.getMimeDecoder();
+        List<Certificate> certList = new ArrayList<>(); // java.security.cert.Certificate
+
+        PrivateKey privateKey = null;
+
+        int start = 0;
+        while (m.find(start)) {
+            String type = m.group(1);
+            String base64Data = m.group(2);
+            byte[] data = decoder.decode(base64Data);
+            start += m.group(0).length();
+            type = type.toUpperCase();
+            if (type.contains(nameTag)) {
+                log.debug("[SSL:Key] Успех, данные найдены: по nameTag = {}, fileResourceName = {}", nameTag, fileResourceName);
+                return data;
+            }
+        }
+        log.error("[SSL:Key] Нет данных по nameTag = {}, fileResourceName = {}", nameTag, fileResourceName);
+        return null;
+    }
+
+    private static SSLContext createSslContext() throws Exception {
+
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        List<Certificate> certList = new ArrayList<>(); // java.security.cert.Certificate
+
+        byte[] certClientBytes = readFileFromPemFile("CERTIFICATE", "/rabbitmq_keys/client_certificate.pem");
+        byte[] certServerBytes = readFileFromPemFile("CERTIFICATE", "/rabbitmq_keys/server_certificate.pem");
+        byte[] privateKeyBytes = readFileFromPemFile("ENCRYPTED PRIVATE KEY", "/rabbitmq_keys/client_key.pem");
+        Certificate clientCert = certFactory.generateCertificate(new ByteArrayInputStream(certClientBytes));
+        certList.add(clientCert);
+
+        Certificate serverCert = certFactory.generateCertificate(new ByteArrayInputStream(certServerBytes));
+        certList.add(serverCert);
+
+
+        EncryptedPrivateKeyInfo pkInfo = new EncryptedPrivateKeyInfo(privateKeyBytes);
+        PBEKeySpec keySpec = new PBEKeySpec("bunnies".toCharArray()); // password
+        SecretKeyFactory pbeKeyFactory = SecretKeyFactory.getInstance(pkInfo.getAlgName());
+        PKCS8EncodedKeySpec encodedKeySpec = pkInfo.getKeySpec(pbeKeyFactory.generateSecret(keySpec));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(encodedKeySpec);
+
+        if (privateKey == null)
+            throw new RuntimeException("RSA private key not found in PEM file");
+
+        char[] keyStorePassword = new char[0];
+
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(null, null);
+
+        int count = 0;
+        for (Certificate cert : certList) {
+            keyStore.setCertificateEntry("cert" + count, cert);
+            count++;
+        }
+        Certificate[] chain = certList.toArray(new Certificate[certList.size()]);
+        keyStore.setKeyEntry("key", privateKey, keyStorePassword, chain);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(keyStore);
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("RSA");
+        kmf.init(keyStore, keyStorePassword);
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+        return sslContext;
+    }
 }
