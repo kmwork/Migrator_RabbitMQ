410b7db3650f930e606900bfbfb6b2ab69fe0b65
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-03 17:27:51 +0300

    делаю shovel_link.sh

--- src/main/resources/kostya-profile/shovel_link.sh
+++ src/main/resources/kostya-profile/shovel_link.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rabbitmqctl set_parameter shovel proba-kostya-migration \
+sudo rabbitmqctl set_parameter shovel proba-kostya-migration \
 '{"src-uri": "amqps://for_ch_root:1@localhost:5671/vhost_ch", "src-queue": "itx_ch_gm_queue_shovel", "dest-uri": "amqps://shovel_user:1@192.168.1.45:5671/vhost_shovel", "dest-queue": "itx_ch_gm_queue_shovel"}'
