b5cce5853abbf53e9787c877c5ab59f02f92df23
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-03 18:47:38 +0300

    сделал shovel_link.sh OK но без SSL - работает

--- src/main/resources/kostya-profile/shovel_link.sh
+++ src/main/resources/kostya-profile/shovel_link.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo rabbitmqctl set_parameter shovel proba-kostya-migration \
-'{"src-uri": "amqps://for_ch_root:1@localhost:5671/vhost_ch", "src-queue": "itx_ch_gm_queue_shovel", "dest-uri": "amqps://shovel_user:1@192.168.1.45:5671/vhost_shovel", "dest-queue": "itx_ch_gm_queue_shovel"}'
+sudo rabbitmqctl -p vhost_ch set_parameter shovel proba-kostya-migration-not-ssl \
+'{"src-uri": "amqp://for_ch_root:1@localhost:5672/vhost_ch", "src-queue": "itx_ch_gm_queue_shovel", "dest-uri": "amqp://shovel_user:1@192.168.1.45:5672/vhost_shovel", "dest-queue": "itx_ch_gm_queue_shovel"}'
+
+
+## amqps://server-name?cacertfile=/path/to/cacert.pem&certfile=/path/to/cert.pem&keyfile=/path/to/key.pem&verify=verify_peer&fail_if_no_peer_cert=true&auth_mechanism=external
+## connect to server-name, with SSL and EXTERNAL authentication
