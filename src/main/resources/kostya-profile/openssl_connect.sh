e92d9a886aaa61218fc39ffdcaf5ae0bd0a813a2
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-04 00:09:15 +0300

    написал утилиту проверки openssl_connect.sh

---
+++ src/main/resources/kostya-profile/openssl_connect.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+local_ssl_dir="/home/papa/work_kac/kFly/src/main/resources/rabbitmq_keys"
+local_cacertfile="$local_ssl_dir/ca_certificate.pem"
+local_certfile="$local_ssl_dir/client_certificate.pem"
+local_keyfile="$local_ssl_dir/client_key.pem"
+local_options="verify=verify_peer&fail_if_no_peer_cert=true&auth_mechanism=external"
+local_password=bunnies
+openssl s_client -connect localhost:5671 -cert $local_certfile -key $local_keyfile -CAfile $local_cacertfile -pass pass:"$local_password"
+
