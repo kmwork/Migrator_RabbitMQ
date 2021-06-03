296ee1cf486cd8686a28df6c66444f0adbe1125c
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-03 21:03:49 +0300

    сделал shovel_link.sh OK но без SSL - работает на ENV переменных

--- src/main/resources/kostya-profile/shovel_link.sh
+++ src/main/resources/kostya-profile/shovel_link.sh
@@ -1,7 +1,30 @@
-#!/bin/bash
+#!/bin/sh
+LOCAL_VHOST=vhost_ch
+LOCAL_USER=for_ch_root
+LOCAL_PASSORD=1
+LOCAL_MQ_HOST=localhost
+LOCAL_MQ_PORT=5672
+LOCAL_MQ_QUEUE=itx_ch_gm_queue_shovel
 
-sudo rabbitmqctl -p vhost_ch set_parameter shovel proba-kostya-migration-not-ssl \
-'{"src-uri": "amqp://for_ch_root:1@localhost:5672/vhost_ch", "src-queue": "itx_ch_gm_queue_shovel", "dest-uri": "amqp://shovel_user:1@192.168.1.45:5672/vhost_shovel", "dest-queue": "itx_ch_gm_queue_shovel"}'
+EXTERNAL_VHOST=vhost_shovel
+EXTERNAL_USER=shovel_user
+EXTERNAL_PASSORD=1
+EXTERNAL_MQ_HOST=192.168.1.45
+EXTERNAL_MQ_PORT=5672
+EXTERNAL_MQ_QUEUE=itx_ch_gm_queue_shovel
+
+
+JSON_VAR='{
+  "src-uri": "amqp://'$LOCAL_USER':'$LOCAL_PASSORD'@'$LOCAL_MQ_HOST':'$LOCAL_MQ_PORT'/'$LOCAL_VHOST'",
+  "src-queue": "'"$LOCAL_MQ_QUEUE"'",
+  "dest-uri": "amqp://'$EXTERNAL_USER':'$EXTERNAL_PASSORD'@'$EXTERNAL_MQ_HOST':'$EXTERNAL_MQ_PORT'/'$EXTERNAL_VHOST'",
+  "dest-queue": "'"$EXTERNAL_MQ_QUEUE"'"
+}'
+echo "============================================"
+echo "$JSON_VAR"
+echo "============================================"
+sudo rabbitmqctl -p vhost_ch set_parameter shovel proba-kostya-migration-not-ssl "$JSON_VAR"
+
 
 
 ## amqps://server-name?cacertfile=/path/to/cacert.pem&certfile=/path/to/cert.pem&keyfile=/path/to/key.pem&verify=verify_peer&fail_if_no_peer_cert=true&auth_mechanism=external
