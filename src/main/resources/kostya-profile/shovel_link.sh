8fc615e71117182dfb317d50900116a45573e637
Konstantin Kakunin <konstantin.kakunin@qligent.com>
2021-06-04 06:02:31 +0300

    попытка 8

--- src/main/resources/kostya-profile/shovel_link.sh
+++ src/main/resources/kostya-profile/shovel_link.sh
@@ -28,25 +28,6 @@ EXTERNAL_MQ_HOST=192.168.1.45
 EXTERNAL_MQ_PORT=5672
 EXTERNAL_MQ_QUEUE=itx_ch_gm_queue_shovel
 
-#jmsUrl="amqps://localhost:5671?transport.trustStoreLocation=$local_keyfile_p12&transport.trustStorePassword=$local_password&transport.verifyHost=false&jms.sendTimeout=5000"
-#echo "jmsUrl=$jmsUrl"
-#JSON_VAR='{
-#  "src-uri": "'"$jmsUrl"'",
-#  "src-queue": "'"$LOCAL_MQ_QUEUE"'",
-#  "dest-uri": "amqp://'$EXTERNAL_USER':'$EXTERNAL_PASSORD'@'$EXTERNAL_MQ_HOST':'$EXTERNAL_MQ_PORT'/'$EXTERNAL_VHOST'",
-#  "dest-queue": "'"$EXTERNAL_MQ_QUEUE"'"
-#}'
-
-#JSON_VAR='{
-#  "src-protocol": "amqp091",
-#  "dest-protocol": "amqp091",
-#  "ack-mode": "on-confirm",
-#  "src-delete-after": "never",
-#  "src-uri": "amqps://'$LOCAL_USER':'$LOCAL_PASSORD'@'$LOCAL_MQ_HOST':'$LOCAL_MQ_PORT'?cacertfile='$local_cacertfile'&certfile='$local_certfile'&keyfile='$local_keyfile'&password='$local_password'&'$local_options'",
-#  "src-queue": "'"$LOCAL_MQ_QUEUE"'",
-#  "dest-uri": "amqp://'$EXTERNAL_USER':'$EXTERNAL_PASSORD'@'$EXTERNAL_MQ_HOST':'$EXTERNAL_MQ_PORT'/'$EXTERNAL_VHOST'",
-#  "dest-queue": "'"$EXTERNAL_MQ_QUEUE"'"
-#}'
 
 JSON_VAR='{
   "src-protocol": "amqp091",
@@ -63,8 +44,3 @@ echo "============================================"
 echo "$JSON_VAR"
 echo "============================================"
 sudo rabbitmqctl -p vhost_ch set_parameter shovel proba-kostya-migration-with-ssl "$JSON_VAR"
-
-
-
-## amqps://server-name?cacertfile=/path/to/cacert.pem&certfile=/path/to/cert.pem&keyfile=/path/to/key.pem&verify=verify_peer&fail_if_no_peer_cert=true&auth_mechanism=external
-## connect to server-name, with SSL and EXTERNAL authentication
